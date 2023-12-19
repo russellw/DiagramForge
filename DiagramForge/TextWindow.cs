6d27e765dfa370d80db595b31e8f3486ecec56e5
Russell Wallace <russell.wallace@gmail.com>
2023-12-19 23:18:17 +0000

    width

--- DiagramForge/TextWindow.cs
+++ DiagramForge/TextWindow.cs
@@ -1,6 +1,9 @@
-﻿namespace DiagramForge {
+﻿using SkiaSharp;
+
+namespace DiagramForge {
 public sealed class TextWindow: Window {
 	public TextLine[] Text;
+	public int TextSize = 12;
 
 	public TextWindow(TextLine[] text) {
 		Text = text;
