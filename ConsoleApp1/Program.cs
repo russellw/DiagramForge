86741140c988b594184bab48e042ce58b9859c87
Russell Wallace <russell.wallace@gmail.com>
2023-12-29 20:09:06 +0000

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,20 @@
-﻿class Program {
-	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+﻿using SkiaSharp;
+
+class Program {
+	static void Main(string[] _) {
+		var width = 200;
+		var height = 100;
+		using var bitmap = new SKBitmap(width, height);
+		using var canvas = new SKCanvas(bitmap);
+		string[] parts = { "int", " ", "x" };
+		using var paint = new SKPaint { TextSize = 12, Color = SKColors.Black };
+		var position = new SKPoint(10, 10);
+		SKColor[] colors = { SKColors.Blue, SKColors.Black, SKColors.White };
+		for (int i = 0; i < parts.Length; i++) {
+			paint.Color = colors[i];
+			canvas.DrawText(parts[i], position.X, position.Y, paint);
+			position.X += paint.MeasureText(parts[i]);
+		}
+		Console.WriteLine(position);
 	}
 }
