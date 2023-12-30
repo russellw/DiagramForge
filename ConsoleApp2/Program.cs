56dd2105b8c5530d8871ca966c794f349399d6a9
Russell Wallace <russell.wallace@gmail.com>
2023-12-30 17:01:32 +0000

    save png

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,15 @@
-﻿class Program {
+﻿using SkiaSharp;
+
+class Program {
 	static void Main(string[] _) {
-		Console.WriteLine("Hello, World!");
+		var width = 1800;
+		var height = 900;
+		using var bitmap = new SKBitmap(width, height);
+		using var canvas = new SKCanvas(bitmap);
+
+		using var image = SKImage.FromBitmap(bitmap);
+		using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+		using var stream = new FileStream("bin/a.png", FileMode.Create);
+		data.SaveTo(stream);
 	}
 }
